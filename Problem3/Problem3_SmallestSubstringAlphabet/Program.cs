b0810ca85234efde1c8a94fe5107ddd46ae8c9e9
megaboy2 <encho_belezirev@outlook.com>
2015-02-24 21:29:15 +0200

    Third task
    Uploading final changes of the third task

---
+++ Problem3/Problem3_SmallestSubstringAlphabet/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_SmallestSubstringAlphabet
+{
+    class Program
+    {
+        const int ALPHABET_LETTERS_COUNT = 26;
+        static void printLetters(List<char> letters)
+        {
+            foreach (char letter in letters)
+            {
+                Console.Write("{0}", letter);
+            }
+        }
+        static List<char> findAlphabetInAnyHalf(int start, int end, string str)
+        {
+            List<char> lettersFound = new List<char>();
+            bool addElement = false;
+            int alphabetLettersFound = 0;
+            for (int index = start; index < end; index++)
+            {
+                if (alphabetLettersFound == ALPHABET_LETTERS_COUNT)
+                {
+                    break;
+                }
+                if (str[index] >= 'a' && str[index] <= 'z')
+                {
+                    lettersFound.Add(str[index]);
+                    addElement = true;
+                    alphabetLettersFound++;
+                }
+                else if (addElement)
+                {
+                    lettersFound.Add(str[index]);
+                }
+
+            }
+
+            return lettersFound;
+        }
+        static int checkHalfSubstring(int startIndex, int endIndex, string str)
+        {
+            int symbolsCounter = 0;
+            for (int index = startIndex; index < endIndex; index++)
+            {
+                if (str[index] >= 'a' && str[index] <= 'z')
+                {
+                    symbolsCounter++;
+                }
+            }
+            return symbolsCounter;
+        }
+        static bool checkForAlphabet(string str)
+        {
+            bool isAlphabet = false;
+            for (int index = 0; index < str.Length; index++)
+            {
+                if (str[index] >= 'a' && str[index] <= 'z')
+                {
+                    int nextLetterAscii = (int)str[index] + 1;
+                    if (str.Contains((char)nextLetterAscii))
+                    {
+                        isAlphabet = true;
+                    }
+                    
+                }
+            }
+            return isAlphabet;
+        }
+        static void smallestSubstringContainingTheAlphabet(string str)
+        {
+            bool isAlphabet = checkForAlphabet(str);
+            if (isAlphabet)
+            {
+                int middle = str.Length / 2;
+                int firstHalfAlphabetLettersCount = checkHalfSubstring(0, middle, str);
+                int secondHalfAlphabetLettersCount = checkHalfSubstring(middle, str.Length, str);
+
+                if (firstHalfAlphabetLettersCount == ALPHABET_LETTERS_COUNT && secondHalfAlphabetLettersCount == ALPHABET_LETTERS_COUNT)
+                {
+                    List<char> lettersInTheFirstHalf = findAlphabetInAnyHalf(0, middle, str);
+                    List<char> lettersInTheScondHalf = findAlphabetInAnyHalf(middle, str.Length, str);
+                    int fakeElementsInFirstHalf = lettersInTheFirstHalf.Count(x => x < 'a' || x > 'z');
+                    int fakeElementsInSecondHalf = lettersInTheScondHalf.Count(x => x < 'a' || x > 'z');
+                    if (fakeElementsInFirstHalf < fakeElementsInSecondHalf)
+                    {
+                        printLetters(lettersInTheFirstHalf);
+                    }
+                    else
+                    {
+                        printLetters(lettersInTheScondHalf);
+                        
+                    }
+                }
+                else if (secondHalfAlphabetLettersCount == ALPHABET_LETTERS_COUNT)
+                {
+                    List<char> letters = findAlphabetInAnyHalf(middle, str.Length, str);
+                    printLetters(letters);
+                }
+                else if (firstHalfAlphabetLettersCount == ALPHABET_LETTERS_COUNT)
+                {
+                    List<char> letters = findAlphabetInAnyHalf(0, middle, str);
+                    printLetters(letters);
+                }
+                else if (firstHalfAlphabetLettersCount != ALPHABET_LETTERS_COUNT && secondHalfAlphabetLettersCount != ALPHABET_LETTERS_COUNT)
+                {
+                    int endIndexForTheFirstHalf = 0;
+                    int startIndexForTheSecondHalf = 0;
+                    for (int index = middle; index < str.Length; index++)
+                    {
+                        if (firstHalfAlphabetLettersCount == ALPHABET_LETTERS_COUNT)
+                        {
+                            endIndexForTheFirstHalf = index + 1;
+                            break;
+                        }
+                        if (str[index] >= 'a' && str[index] <= 'z')
+                        {
+                            firstHalfAlphabetLettersCount++;
+                        }
+                    }
+
+                    List<char> lettersInFirstHalf = findAlphabetInAnyHalf(0, endIndexForTheFirstHalf, str);
+
+                    for (int index = middle; index >= 0; index--)
+                    {
+                        if (secondHalfAlphabetLettersCount == ALPHABET_LETTERS_COUNT)
+                        {
+                            startIndexForTheSecondHalf = index;
+                            break;
+                        }
+                        if (str[index] >= 'a' && str[index] <= 'z')
+                        {
+                            secondHalfAlphabetLettersCount++;
+                        }
+
+                    }
+
+                    List<char> lettersInSecondHalf = findAlphabetInAnyHalf(startIndexForTheSecondHalf, str.Length, str);
+                    int fakeElementsInFirstHalf = lettersInFirstHalf.Count(x => x < 'a' || x > 'z');
+                    int fakeElementsInSecondHalf = lettersInSecondHalf.Count(x => x < 'a' || x > 'z');
+                    if (fakeElementsInFirstHalf < fakeElementsInSecondHalf)
+                    {
+                        printLetters(lettersInFirstHalf);
+                        
+                    }
+                    else
+                    {
+                        printLetters(lettersInSecondHalf);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no alphabet in the input string");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string inputString = Console.ReadLine();
+            smallestSubstringContainingTheAlphabet(inputString);
+            Console.WriteLine();
+        }
+    }
+}
